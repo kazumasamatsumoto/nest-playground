34243c623d9a461fba3ea54bce140d4d4555aa14
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-04-21 15:49:31 +0900

    dtoはJavaScriptの環境でも型定義が可能になるツールです

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,9 +1,3 @@
-export class CreateCoffeeDto {
-  readonly name: string;
-  readonly brand: string;
-  readonly flavors: string[];
-}
-
 export class UpdateCoffeeDto {
   readonly name?: string;
   readonly brand?: string;
